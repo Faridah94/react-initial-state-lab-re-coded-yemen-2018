dd205ed98fc01f80b971c208022a3ecba2664c7a
Farida <faridah.absi@gmail.com>
2018-10-27 20:22:23 +0000

    automatically backed up by learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -11,12 +11,22 @@ export default class ImageSlider extends React.Component {
   }
 
   render() {
+<<<<<<< HEAD
     const sms =       `I am on slide ${this.state.currentSlideIndex}`;
 
     return (
       <div>
       {sms}
+=======
+    return (
+      <div>
+      I am on slide {this.state.currentSlideIndex};
+>>>>>>> 18e5dd176e91def7fdc96620925ffc616f05954a
       </div>
     )
   }
 }
+<<<<<<< HEAD
+=======
+
+>>>>>>> 18e5dd176e91def7fdc96620925ffc616f05954a
